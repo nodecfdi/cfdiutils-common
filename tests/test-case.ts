4fe8d550c4099c7d816ce937af8e07f0e9171ae8
Fernando Isidro L <luffynando@gmail.com>
2022-07-01 01:42:51 -0500

    chore: update new configuration for bundle and better quality code

--- tests/test-case.ts
+++ tests/test-case.ts
@@ -1,14 +1,7 @@
 import { join } from 'path';
 
-const useTestCase = (): {
-    utilAsset(file: string): string;
-} => {
-    const utilAsset = (file: string): string => {
-        return join(__dirname, 'assets', file);
-    };
-
-    return {
-        utilAsset,
-    };
-};
-export { useTestCase };
+export class TestCase {
+    public static utilAsset(file: string): string {
+        return join(__dirname, '_files', file);
+    }
+}
